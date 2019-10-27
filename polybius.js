e22a42d21a4c299265924cb47872cab6fa084341
Aaron Lin <aaronlin256@gmail.com>
2019-10-26 20:22:18 -0400

    Better permutation of results

--- polybius.js
+++ polybius.js
@@ -1,18 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var _ = require("lodash");
 function decodePolybius(input) {
-    var firstAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    var secondAlphabet = 'abcdefghjklmnopqrstuvwxyz'.split('');
-    var polybiusSquareWithLetterI = initializePolybiusSquare(firstAlphabet);
-    var polybiusSquareWithLetterJ = initializePolybiusSquare(secondAlphabet);
+    var alphabet = 'abcdefghiklmnopqrstuvwxyz'.split('');
+    var polybiusSquare = initializePolybiusSquare(alphabet);
     var firstPotentialDecode = '';
-    var secondPotentialDecode = '';
     var separatedInput = input.match(/..?/g);
     for (var _i = 0, separatedInput_1 = separatedInput; _i < separatedInput_1.length; _i++) {
         var grouping = separatedInput_1[_i];
         var _a = grouping.split(''), i = _a[0], j = _a[1];
-        firstPotentialDecode += polybiusSquareWithLetterI[parseInt(i) - 1][parseInt(j) - 1];
-        secondPotentialDecode += polybiusSquareWithLetterJ[parseInt(i) - 1][parseInt(j) - 1];
+        firstPotentialDecode += polybiusSquare[parseInt(i) - 1][parseInt(j) - 1];
     }
-    return [firstPotentialDecode, secondPotentialDecode];
+    return _.uniq(_.flatten([firstPotentialDecode, permutateDecode(firstPotentialDecode)]));
 }
 function initializePolybiusSquare(alphabet) {
     var polybiusSquare = [[]];
@@ -26,5 +25,25 @@ function initializePolybiusSquare(alphabet) {
     }
     return polybiusSquare;
 }
-var decode = decodePolybius('1114153115');
-console.log(decode);
+function permutateDecode(output) {
+    var characters = output.split('');
+    var permutations = [output];
+    for (var index = 0; index < characters.length; index++) {
+        var newOutput = '';
+        switch (characters[index]) {
+            case 'i':
+                newOutput = output.substr(0, index) + 'j' + output.substring(index + 1);
+                permutations.push(newOutput);
+                break;
+            case 'j':
+                newOutput = output.substr(0, index) + 'i' + output.substring(index + 1);
+                permutations.push(newOutput);
+                break;
+        }
+    }
+    return permutations;
+}
+var decodedValue = decodePolybius('1114153115');
+console.log(decodedValue);
+var edgeCase = decodePolybius('24111325');
+console.log(edgeCase);
--- polybius.ts
+++ polybius.ts
@@ -1,20 +1,18 @@
+import * as _ from 'lodash'
+
 function decodePolybius(input: string) {
-    const firstAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const secondAlphabet = 'abcdefghjklmnopqrstuvwxyz'.split('');
-    let polybiusSquareWithLetterI: string[][] = initializePolybiusSquare(firstAlphabet)
-    let polybiusSquareWithLetterJ: string[][] = initializePolybiusSquare(secondAlphabet)
+    const alphabet = 'abcdefghiklmnopqrstuvwxyz'.split('');
+    let polybiusSquare: string[][] = initializePolybiusSquare(alphabet)
     
     let firstPotentialDecode = ''
-    let secondPotentialDecode = ''
 
     let separatedInput = input.match(/..?/g)
     for(let grouping of separatedInput) {
         let [i, j] = grouping.split('')
-        firstPotentialDecode += polybiusSquareWithLetterI[parseInt(i) - 1][parseInt(j) - 1]
-        secondPotentialDecode += polybiusSquareWithLetterJ[parseInt(i) - 1][parseInt(j) - 1]
+        firstPotentialDecode += polybiusSquare[parseInt(i) - 1][parseInt(j) - 1]
     }
 
-    return [firstPotentialDecode, secondPotentialDecode]
+    return _.uniq(_.flatten([firstPotentialDecode, permutateDecode(firstPotentialDecode)]))
 
 }
 
@@ -31,5 +29,27 @@ function initializePolybiusSquare(alphabet: string[]) {
     return polybiusSquare
 }
 
+function permutateDecode(output: string) {
+    let characters = output.split('');
+    let permutations = [output]
+    for(let index = 0; index < characters.length; index++) {
+        let newOutput = ''
+        switch (characters[index]) {
+            case 'i':
+                newOutput = output.substr(0, index) + 'j' + output.substring(index + 1)
+                permutations.push(newOutput)
+                break
+            case 'j':
+                newOutput = output.substr(0, index) + 'i' + output.substring(index + 1)
+                permutations.push(newOutput)
+                break
+        }
+    }
+    return permutations
+}
+
 const decodedValue = decodePolybius('1114153115')
 console.log(decodedValue)
+
+const edgeCase = decodePolybius('24111325')
+console.log(edgeCase)
